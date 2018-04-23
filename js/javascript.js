2f385f2f0f2ab2a8c77646e6b79986ebfac61dfb
Michal Goluch <goluch.michal89@gmail.com>
2018-04-23 21:25:18 +0200

    create class for card

--- js/javascript.js
+++ js/javascript.js
@@ -42,4 +42,31 @@ console.log(randomString());
         }
     }
 
+    Column.prototype = {
+        addCard: function(card) {
+            this.$element.children("ul").append(card.$element);
+        },
+        removeColumn: function() {
+            this.$element.remove();
+        }
+    }
+
+    function Card(description) {
+        var self = this;
+    
+        this.id = randomString();
+        this.description = description;
+        this.$element = createCard();
+    
+        function createCard() {
+            var $card = $("<li>").addClass("card");
+            var $cardDescription = $("<p>").addClass("card-description").text(self.description);
+            var $cardDelete = $("<button>").addClass("btn-delete").text("x");
+
+            $cardDelete.click(function(){
+                self.removeCard();
+            });
+        }
+    }
+
 });
