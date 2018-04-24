b95fc4747fe5555c77baa786d76d777e9cb2961a
Michal Goluch <goluch.michal89@gmail.com>
2018-04-24 18:24:48 +0200

    test the project

--- js/javascript.js
+++ js/javascript.js
@@ -78,5 +78,27 @@ console.log(randomString());
             this.$element.remove();
         }
     }
+    
+    var board = {
+        name: "Kanban Board",
+        addColumn: function(column) {
+            this.$element.append(column.$element);
+            initSortable();
+        },
+        $element: $("#board .column-container")
+    };
+
+    $(".create-column").click(function(){
+        var name = prompt("Enter a column name");
+        var column = new Column(name);
+        board.addColumn(column);
+    });
+
+    function initSortable() {
+        $(".column-card-list").sortable({
+            connectWith: ".column-card-list",
+            placeholder: "card-placeholder"
+        }).disableSelection();
+    }
 
 });
