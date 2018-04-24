1d5c1083ac3916a6d9316fdb658a599484e7c852
Michal Goluch <goluch.michal89@gmail.com>
2018-04-24 20:19:29 +0200

    add method responsible for calculate the phone warranty

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,27 @@
+
+
 function Phone(brand, price, color) {
     this.brand = brand;
     this.price = price;
     this.color = color;
+    this.getWarrantyCost = function() {
+        return 0.1 * this.price;
+    };
 }
 
+
+
 Phone.prototype.printinfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
+    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". Extended warranty for this model cost " + this.getWarrantyCost());
+};
+
 
 var xiaomiRedmi = new Phone("Xiaomi", 1000, "black");
 var samsungGalaxy = new Phone("Samsung", 2000, "golden");
 var huaweiP = new Phone("Huawei", 1500, "white");
+
+
+
 
 xiaomiRedmi.printinfo();
 samsungGalaxy.printinfo();
