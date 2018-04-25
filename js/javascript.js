7ea19f4bbbfe583f8154173b17c8dfef901998dd
Michal Goluch <goluch.michal89@gmail.com>
2018-04-25 19:48:28 +0200

    move warranty method to the prototype

--- js/javascript.js
+++ js/javascript.js
@@ -4,14 +4,14 @@ function Phone(brand, price, color) {
     this.brand = brand;
     this.price = price;
     this.color = color;
-    this.getWarrantyCost = function() {
-        return 0.1 * this.price;
-    };
 }
 
 
 
 Phone.prototype.printinfo = function() {
+    this.getWarrantyCost = function() {
+        return 0.1 * this.price;
+    };
     console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ". Extended warranty for this model cost " + this.getWarrantyCost());
 };
 
